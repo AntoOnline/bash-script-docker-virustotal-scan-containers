8420b816356e93b1b9010bdd23f29bc889f22067
Anthony Mastoroudes <anthony.mast@gmail.com>
2023-05-01 17:06:43 +1000

    first commit

---
+++ readme.md
@@ -0,0 +1,99 @@
+# Docker Backup and VirusTotal Scanner
+
+This is a Bash script that backs up Docker images or containers, and then scans them using VirusTotal. The script uploads the Docker tar files to VirusTotal for analysis. Please read Virus Total's terms of service before using this script.
+
+## Disclaimer
+
+1. Use this code at your own risk.
+2. The file is uploaded to VirusTotal for analysis. Please read Virus Total's terms of service before using this script.
+3. VirusTotal shares uploaded files with their partners, which may include antivirus companies, researchers, and other organizations. Be cautious when uploading sensitive or proprietary files.
+
+## How to get a VirusTotal API Key
+
+To use this script, you will need a VirusTotal API key. Follow these steps to obtain one:
+
+1. Visit [VirusTotal](https://www.virustotal.com/) and sign up for a free account.
+2. After signing up, log in to your account.
+3. Navigate to your [API Key](https://www.virustotal.com/gui/user/YOUR_USERNAME/apikey) page by clicking your username in the top-right corner and selecting "API Key."
+4. Your API key will be displayed on the page. Copy it and use it as the `VIRUS_TOTAL_API_KEY` value when running the script.
+
+## Usage
+
+```bash
+./vt_scan_containers.sh --BASE_FOLDER=PATH --VIRUS_TOTAL_API_KEY=KEY --EXPORT_TYPE=[image/container] [--SLACK_WEB_HOOK=URL] [--DEBUG]
+```
+
+Example:
+
+```
+./vt_scan_containers.sh --BASE_FOLDER=/mnt/container_backups/ --VIRUS_TOTAL_API_KEY=e4c0f729f84EXAMPLE539a280000000 --EXPORT_TYPE=container --SLACK_WEB_HOOK=https://hooks.slack.com/services/example/example/example
+```
+
+### Options
+
+- `--BASE_FOLDER`: Path to the folder where the Docker backups and results will be stored.
+- `--VIRUS_TOTAL_API_KEY`: Your VirusTotal API key.
+- `--EXPORT_TYPE`: Export type can be either `image` or `container`.
+- `--SLACK_WEB_HOOK` (Optional): Slack webhook URL to send notifications.
+- `--DEBUG` (Optional): Run the script in debug mode. This will only do one container/image as a test run.
+
+## Dependencies
+
+- Docker
+- cincan/virustotal Docker image
+
+Make sure you have Docker installed and the `cincan/virustotal` Docker image available.
+
+## How It Works
+
+1. The script first checks for the required dependencies.
+2. It exports the Docker images or containers based on the provided `EXPORT_TYPE`.
+3. The exported tar files are scanned using the VirusTotal API.
+4. The script waits for VirusTotal to analyze the files.
+5. The analysis results are checked for malicious or suspicious content.
+6. Notifications are sent to the provided Slack webhook URL if any malicious or suspicious content is detected.
+
+Example console output:
+
+```
+Docker save container dcl to /mnt/container_backups/dcl.tar
+Docker save container portainer to /mnt/container_backups/portainer.tar
+Upload tar file to VirusTotal for scanning: dcl.tar
+Received result: /files/dcl.tar YmVlMDliODFjMjkwZDk1MmQ5YTk3Nj==
+Upload tar file to VirusTotal for scanning: portainer.tar
+Received result: /files/portainer.tar MzhlMjM0ZDY1NTA2N2IxNjkwM==
+Sleeping for 1 minute to give VirusTotal time to scan the file.
+Analyzing result file: dcl.tar.result
+VirusTotal is still scanning. Retrying in 10 seconds - 0/30
+VirusTotal is still scanning. Retrying in 10 seconds - 1/30
+VirusTotal is still scanning. Retrying in 10 seconds - 2/30
+VirusTotal is still scanning. Retrying in 10 seconds - 3/30
+VirusTotal is still scanning. Retrying in 10 seconds - 4/30
+VirusTotal is still scanning. Retrying in 10 seconds - 5/30
+VirusTotal is still scanning. Retrying in 10 seconds - 6/30
+VirusTotal is still scanning. Retrying in 10 seconds - 7/30
+VirusTotal is still scanning. Retrying in 10 seconds - 8/30
+VirusTotal is still scanning. Retrying in 10 seconds - 9/30
+VirusTotal is still scanning. Retrying in 10 seconds - 10/30
+VirusTotal is still scanning. Retrying in 10 seconds - 11/30
+VirusTotal is still scanning. Retrying in 10 seconds - 12/30
+VirusTotal is still scanning. Retrying in 10 seconds - 13/30
+VirusTotal is still scanning. Retrying in 10 seconds - 14/30
+VirusTotal is still scanning. Retrying in 10 seconds - 15/30
+VirusTotal is still scanning. Retrying in 10 seconds - 16/30
+VirusTotal is still scanning. Retrying in 10 seconds - 17/30
+No malicious or suspicious file found in dcl.tar.
+Analyzing result file: portainer.tar.result
+No malicious or suspicious file found in portainer.tar.
+```
+
+## Limitations
+
+Currently, only `EXPORT_TYPE=container` is fully supported. Image uploads are possible, but the status always remains "queued".
+```
+
+## Want to connect?
+
+Feel free to contact me on [Twitter](https://twitter.com/OnlineAnto), [DEV Community](https://dev.to/antoonline/) or [LinkedIn](https://www.linkedin.com/in/anto-online) if you have any questions or suggestions.
+
+Or just visit my [website](https://anto.online) to see what I do.
---
+++ vt_scan_containers.sh
@@ -0,0 +1,266 @@
+#!/bin/bash
+# Script to backup docker images and containers and scan them with VirusTotal
+#
+# Currently only EXPORT_TYPE=container is supported. Image uploads but the status always remains "queued".
+#
+# Disclaimer :
+#
+# 1) Use this code at your own risk.
+# 2) The file is uploaded to VirusTotal for analysis. Please read Virus Total's terms of service before using this script.
+#
+# Visit http://anto.online for more information or give a like on GitHub: https://github.com/yourusername/yourrepository
+#
+
+# Docker image to use for VirusTotal scanning
+VIRUS_TOTAL_DOCKER_IMAGE="cincan/virustotal"
+
+# Max bytes to scan (600MB)
+MAX_FILE_SIZE_TO_SCAN=629145600
+
+# Max number of retries to check the scan status
+MAX_RETRIES=16
+
+# Number of seconds to wait between retries
+RETRY_INTERVAL=15
+
+# Sleep seconds after uploading all files to VirusTotal
+SLEEP_TIME_AFTER_ALL_FILES_UPLOADED=30
+
+# Emojis
+VIRUS_FOUND=$(echo -e "\U2623")
+NO_VIRUS_FOUND=$(echo -e "\U2714")
+
+# Function to print usage instructions
+print_usage() {
+  echo "Usage: $0 --BASE_FOLDER=PATH --VIRUS_TOTAL_API_KEY=KEY --EXPORT_TYPE=[image/container] [--SLACK_WEB_HOOK=URL] [--DEBUG]"
+  exit 1
+}
+
+# Function to sanitize the item id and remove / space and dots
+sanitize_item_id() {
+    local item_id="$1"
+    local item_id_safe
+
+    # Remove / and : and . and spaces
+    item_id_safe=$(echo "$item_id" | tr -d '/: .')
+
+    # Remove she word "sha" from the beginning of the string
+    item_id_safe=$(echo "$item_id_safe" | sed 's/^sha//')
+
+    echo "$item_id_safe"
+}
+
+# Function to backup a docker item (image or container)
+backup_docker_item() {
+    local EXPORT_TYPE="$1"
+    local item_id="$2"
+    local BASE_FOLDER="$3"
+    local item_id_safe="$4"
+
+    if [ "$EXPORT_TYPE" = "image" ]; then
+        # Images somehow always return resource not found when using docker save - this is a workaround
+        # instead of: docker save "$item_id" > "${BASE_FOLDER}${item_id_safe}.tar"
+        # The docker create command initializes a container based on the specified image, but it doesn't start the container or run any processes in it.
+        echo "Creating a temporary container from image $item_id"
+        local container_id=$(docker create "$item_id")
+
+        echo "Docker export container $container_id (from image $item_id) to ${BASE_FOLDER}${item_id_safe}.tar"
+        docker export "$container_id" > "${BASE_FOLDER}${item_id_safe}.tar"
+
+        echo "Removing temporary container $container_id"
+        docker rm "$container_id"
+    elif [ "$EXPORT_TYPE" = "container" ]; then
+        echo "Docker save container $item_id to ${BASE_FOLDER}${item_id_safe}.tar"
+        docker export "$item_id" > "${BASE_FOLDER}${item_id_safe}.tar"
+    fi
+}
+
+# Function send a Slack notification
+send_slack_notification() {
+    local SLACK_WEB_HOOK="$1"
+    local slack_message="$2"
+
+    if [[ -n "$SLACK_WEB_HOOK" ]]; then
+        curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$slack_message\"}" "$SLACK_WEB_HOOK"
+    fi
+}
+
+# Parse arguments
+for arg in "$@"; do
+  case $arg in
+    --BASE_FOLDER=*)
+      BASE_FOLDER="${arg#*=}"
+      shift
+      ;;
+    --VIRUS_TOTAL_API_KEY=*)
+      VIRUS_TOTAL_API_KEY="${arg#*=}"
+      shift
+      ;;
+    --EXPORT_TYPE=*)
+      EXPORT_TYPE="${arg#*=}"
+      shift
+      ;;
+    --SLACK_WEB_HOOK=*)
+      SLACK_WEB_HOOK="${arg#*=}"
+      shift
+      ;;
+    --DEBUG)
+      DEBUG=true
+      shift
+      ;;
+    *)
+      print_usage
+      ;;
+  esac
+done
+
+# Check if BASE_FOLDER and VIRUS_TOTAL_API_KEY are provided
+if [[ -z "$BASE_FOLDER" || -z "$VIRUS_TOTAL_API_KEY" || -z "$EXPORT_TYPE" ]]; then
+  print_usage
+fi
+
+# Check if EXPORT_TYPE is valid
+if [[ "$EXPORT_TYPE" != "image" && "$EXPORT_TYPE" != "container" ]]; then
+  echo "Invalid EXPORT_TYPE. Exiting."
+  exit 1
+fi
+
+# Make sure BASE_FOLDER ends with a slash
+if [[ "$BASE_FOLDER" != */ ]]; then
+  BASE_FOLDER="${BASE_FOLDER}/"
+fi
+
+# Set DEBUG to false if not provided
+DEBUG=${DEBUG:-false}
+
+# Check if Docker is installed
+if ! command -v docker &> /dev/null; then
+  echo "Docker is not installed. Exiting."
+  exit 1
+fi
+
+# Delete and recreate the BASE_FOLDER
+rm -rf "$BASE_FOLDER"
+mkdir -p "$BASE_FOLDER"
+chmod 555 -R "$BASE_FOLDER"
+
+# Export image or container
+format_string="{{.Names}}"
+if [ "$EXPORT_TYPE" = "image" ]; then
+    # Images do not contain the mounted volumes
+    format_string="{{.ID}}"
+elif [ "$EXPORT_TYPE" = "container" ]; then
+    format_string="{{.Names}}"
+fi
+
+if [ "$DEBUG" = true ]; then
+  if [ "$EXPORT_TYPE" = "image" ]; then
+    item_id=$(docker ps --format "$format_string" | xargs docker inspect --format '{{.Image}}' | sort -u | head -n 1)
+  else
+    item_id=$(docker ps --format "$format_string" | head -n 1)
+  fi
+  item_id_safe=$(sanitize_item_id "$item_id")
+  backup_docker_item "$EXPORT_TYPE" "$item_id" "$BASE_FOLDER" "$item_id_safe"
+else
+  if [ "$EXPORT_TYPE" = "image" ]; then
+    item_ids=$(docker ps --format "$format_string" | xargs docker inspect --format '{{.Image}}' | sort -u)
+  else
+    item_ids=$(docker ps --format "$format_string" | sort -u)
+  fi
+
+  for item_id in $item_ids; do
+    item_id_safe=$(sanitize_item_id "$item_id")
+
+    if [ "$EXPORT_TYPE" = "image" ]; then
+      if docker image inspect "$item_id" > /dev/null 2>&1; then
+          echo "Image $item_id exists."
+      else
+          echo "Image $item_id not found. Skipping export."
+          continue
+      fi
+    fi
+
+    backup_docker_item "$EXPORT_TYPE" "$item_id" "$BASE_FOLDER" "$item_id_safe"
+  done
+fi
+
+# Scan the exported tar files with VirusTotal
+for tar_file in "${BASE_FOLDER}"*.tar; do
+    # check if the file is larger than xMB for the public api, if so, skip it
+    if [[ $(stat -c%s "$tar_file") -gt $MAX_FILE_SIZE_TO_SCAN ]]; then
+        echo "Skipping and removing $tar_file as it is larger than $MAX_FILE_SIZE_TO_SCAN bytes."
+        rm "$tar_file"
+        continue
+    fi
+
+    tar_file_name=$(basename "$tar_file")
+    result_file="${tar_file}.result"
+    analysis_file="${result_file}.analysis"
+
+    # Perform VirusTotal scan
+    echo "Upload tar file to VirusTotal for scanning: $tar_file_name"
+    result=$(docker run --rm -v "${BASE_FOLDER}:/files" $VIRUS_TOTAL_DOCKER_IMAGE -k $VIRUS_TOTAL_API_KEY scan file "/files/${tar_file_name}")
+    echo "Received result: ${result}"
+
+    # Check if the result contains one space and two words, if not, exit the script and send a Slack notification
+    if [[ $(echo "$result" | wc -w) -ne 2 ]]; then
+        slack_message="Result not received for $tar_file_name."
+        printf "%s\n" "$slack_message"
+        send_slack_notification $SLACK_WEB_HOOK "${slack_message}"
+        continue
+    fi
+    echo "$result" > $result_file
+done
+
+echo "Sleeping for $SLEEP_TIME_AFTER_ALL_FILES_UPLOADED seconds to give VirusTotal time to scan the file."
+sleep $SLEEP_TIME_AFTER_ALL_FILES_UPLOADED
+
+# Scan the exported tar files with VirusTotal
+for tar_file in "${BASE_FOLDER}"*.tar; do
+    tar_file_name=$(basename "${tar_file}")
+    result_file="${tar_file}.result"
+
+    # Loop until the text 'status: "completed"' is found in the result file
+    printf "Analyzing result file: %s\n" "$(basename "$result_file")"
+    result_content=$(cat "$result_file" | tr -d '\r')
+
+    retries=0
+    while [ "$retries" -lt "$MAX_RETRIES" ]; do
+        analysis=$(docker run --rm -v "${BASE_FOLDER}:/files" $VIRUS_TOTAL_DOCKER_IMAGE -k $VIRUS_TOTAL_API_KEY analysis $result_content)
+        printf "%s\n" "$analysis" > "$analysis_file"
+
+        if grep -q "status" "$analysis_file" && grep -q "completed" "$analysis_file"; then
+            break
+        else
+            printf "VirusTotal is still scanning. Retrying in $RETRY_INTERVAL seconds - %s/%s\n" "$retries" "$MAX_RETRIES"
+            sleep $RETRY_INTERVAL
+            retries=$((retries + 1))
+        fi
+    done
+
+    if [ "$retries" -eq "$MAX_RETRIES" ]; then
+        echo "Error: Reached the maximum number of retries. Skip."
+        continue
+    fi
+
+    # Double check if the analysis file was successfully created by looking for the word 'result'
+    if ! grep -q "result" "$analysis_file"; then
+        slack_message="Analysis file not created for $tar_file_name."
+        printf "%s\n" "$slack_message"
+        send_slack_notification $SLACK_WEB_HOOK "${slack_message}"
+        continue
+    fi
+
+    # Check for malicious or suspicious results and send a Slack notification or print the result
+    if ! (grep -q "malicious: 0" "$analysis_file" && grep -q "suspicious: 0" "$analysis_file"); then
+        slack_message="$VIRUS_FOUND Possible malicious or suspicious file in:\n\`\`\`$(cat "$analysis_file")\`\`\`"
+        printf "%s\n" "$slack_message"
+        send_slack_notification $SLACK_WEB_HOOK "${slack_message}"
+        printf "\n"
+
+    else
+        printf "$NO_VIRUS_FOUND No malicious or suspicious file found in %s.\n" "$tar_file_name"
+        printf "\n"
+
+    fi
+done
